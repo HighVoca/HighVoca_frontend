647038580d1b148761fdc04ffabdbed33d4a6af1
P1su <pissssssu@naver.com>
2026-03-07 21:18:54 +0900

    feat: WordCard key 값을 통하여 isClicked 상태 초기화

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -4,89 +4,90 @@ import WordTab from './components/WordTab/WordTab';
 import WordCard from './components/WordCard/WordCard';
 
 export default function Main() {
-	const [clickedCategory, setClickedCategory] = useState<string>('learned');
-	const [clickedLanguage, setClickedLanguage] = useState<string>('All');
-	const wordCategories = [
-		{
-			key: 'learned',
-			label: '학습한 단어',
-		},
-		{
-			key: 'all',
-			label: '모든 단어',
-		},
-		{
-			key: 'wrong',
-			label: '틀린 단어',
-		},
-	];
-	const languageCategories = [
-		{
-			key: 'English',
-			label: '영어만',
-		},
-		{
-			key: 'Korean',
-			label: '한글만',
-		},
-		{
-			key: 'All',
-			label: '모두',
-		},
-	];
+  const [clickedCategory, setClickedCategory] = useState<string>('learned');
+  const [clickedLanguage, setClickedLanguage] = useState<string>('All');
+  const wordCategories = [
+    {
+      key: 'learned',
+      label: '학습한 단어',
+    },
+    {
+      key: 'all',
+      label: '모든 단어',
+    },
+    {
+      key: 'wrong',
+      label: '틀린 단어',
+    },
+  ];
+  const languageCategories = [
+    {
+      key: 'English',
+      label: '영어만',
+    },
+    {
+      key: 'Korean',
+      label: '한글만',
+    },
+    {
+      key: 'All',
+      label: '모두',
+    },
+  ];
 
-	const mockData = [
-		{
-			meaning: '사과',
-			spelling: 'apple',
-			isWrong: false,
-		},
-		{
-			meaning: '행복한',
-			spelling: 'happy',
-			isWrong: false,
-		},
-		{
-			meaning: '아름다운',
-			spelling: 'beautiful',
-			isWrong: true,
-		},
-		{
-			meaning: '산',
-			spelling: 'mountain',
-			isWrong: false,
-		},
-		{
-			meaning: '도서관',
-			spelling: 'library',
-			isWrong: false,
-		},
-	];
+  const mockData = [
+    {
+      meaning: '사과',
+      spelling: 'apple',
+      isWrong: false,
+    },
+    {
+      meaning: '행복한',
+      spelling: 'happy',
+      isWrong: false,
+    },
+    {
+      meaning: '아름다운',
+      spelling: 'beautiful',
+      isWrong: true,
+    },
+    {
+      meaning: '산',
+      spelling: 'mountain',
+      isWrong: false,
+    },
+    {
+      meaning: '도서관',
+      spelling: 'library',
+      isWrong: false,
+    },
+  ];
 
-	return (
-		<S.MainLayout>
-			<S.TabContainer>
-				<WordTab
-					categories={wordCategories}
-					clickedItem={clickedCategory}
-					setClickedItem={setClickedCategory}
-					size='large'
-				/>
-				<WordTab
-					categories={languageCategories}
-					clickedItem={clickedLanguage}
-					setClickedItem={setClickedLanguage}
-					size='small'
-				/>
-			</S.TabContainer>
-			{mockData.map(({ meaning, spelling, isWrong }) => (
-				<WordCard
-					meaning={meaning}
-					spelling={spelling}
-					isWrong={isWrong}
-					languageType={clickedLanguage}
-				/>
-			))}
-		</S.MainLayout>
-	);
+  return (
+    <S.MainLayout>
+      <S.TabContainer>
+        <WordTab
+          categories={wordCategories}
+          clickedItem={clickedCategory}
+          setClickedItem={setClickedCategory}
+          size='large'
+        />
+        <WordTab
+          categories={languageCategories}
+          clickedItem={clickedLanguage}
+          setClickedItem={setClickedLanguage}
+          size='small'
+        />
+      </S.TabContainer>
+      {mockData.map(({ meaning, spelling, isWrong }) => (
+        <WordCard
+          key={`${clickedCategory}-${clickedLanguage}-${spelling}`}
+          meaning={meaning}
+          spelling={spelling}
+          isWrong={isWrong}
+          languageType={clickedLanguage}
+        />
+      ))}
+    </S.MainLayout>
+  );
 }
